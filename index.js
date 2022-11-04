5177ddc11f6ce2fdbceff949a789e100774a53c4
wafu <rhfemtlfem@naver.com>
2022-11-04 16:25:13 +0900

    Update: list delete event

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 console.log("TS파일 실행");
-var todoInputbox = window.document.getElementsByTagName("form")[0];
-console.log(document.getElementById("todoList"));
+var todoInputbox = document.getElementsByTagName("form")[0];
 var todoList = document.getElementById("todoList");
-var handlesbmit = function (event) {
+var buttons = document.getElementsByTagName("button")[0];
+// 인풋박스에 텍스트 엔터 시 리스트 추가
+var handlesubmitAdd = function (event) {
     event.preventDefault();
-    var inputValue = window.document.querySelectorAll("input")[0].value;
-    var createLi = window.document.createElement("li");
-    createLi.textContent = inputValue;
+    // 텍스트를 리스트에 추가
+    var inputValue = document.querySelectorAll("input")[0].value;
+    var createLi = document.createElement("li");
+    createLi.textContent = "".concat(inputValue, " ");
     todoList.appendChild(createLi);
+    // 삭제 버튼을 추가
+    var createButton = document.createElement("button");
+    createButton.textContent = "X";
+    createButton.addEventListener("click", handleclickDelete);
+    createLi.appendChild(createButton);
     window.document.querySelectorAll("input")[0].value = "";
 };
-todoInputbox.addEventListener("submit", handlesbmit);
+// 버튼 클릭 시 삭제
+var handleclickDelete = function (event) {
+    event.target.parentElement.remove();
+};
+todoInputbox.addEventListener("submit", handlesubmitAdd);
--- index.ts
+++ index.ts
@@ -1,14 +1,31 @@
 console.log("TS파일 실행");
-const todoInputbox = window.document.getElementsByTagName("form")[0];
-console.log(document.getElementById("todoList"));
+
+const todoInputbox = document.getElementsByTagName("form")[0];
+
 const todoList = document.getElementById("todoList");
-const handlesbmit = (event) => {
+const buttons = document.getElementsByTagName("button")[0];
+
+// 인풋박스에 텍스트 엔터 시 리스트 추가
+const handlesubmitAdd = (event) => {
   event.preventDefault();
-  let inputValue = window.document.querySelectorAll("input")[0].value;
-  const createLi = window.document.createElement("li");
-  createLi.textContent = inputValue;
+  // 텍스트를 리스트에 추가
+  let inputValue = document.querySelectorAll("input")[0].value;
+  const createLi = document.createElement("li");
+  createLi.textContent = `${inputValue} `;
   todoList.appendChild(createLi);
+
+  // 삭제 버튼을 추가
+  const createButton = document.createElement("button");
+  createButton.textContent = "X";
+  createButton.addEventListener("click", handleclickDelete);
+  createLi.appendChild(createButton);
+
   window.document.querySelectorAll("input")[0].value = "";
 };
 
-todoInputbox.addEventListener("submit", handlesbmit);
+// 버튼 클릭 시 삭제
+const handleclickDelete = (event) => {
+  event.target.parentElement.remove();
+};
+
+todoInputbox.addEventListener("submit", handlesubmitAdd);
